49ad162dd8ebe23c3cd664a1546e1b03d70ef03c
Simaab Malik <seemabmalik2005@gmail.com>
2025-12-29 03:42:16 +0500

    Add files via upload

--- Code-Alpha (Frontend)/image-gallery/script.js
+++ Code-Alpha (Frontend)/image-gallery/script.js
@@ -1,1 +1,17 @@
+let currentIndex = 0;
+const images = document.querySelectorAll('.gallery img');
 
+function openLightbox(img) {
+    document.getElementById('lightbox').style.display = 'flex';
+    document.getElementById('lightbox-img').src = img.src;
+    currentIndex = Array.from(images).indexOf(img);
+}
+
+function closeLightbox() {
+    document.getElementById('lightbox').style.display = 'none';
+}
+
+function changeImage(step) {
+    currentIndex = (currentIndex + step + images.length) % images.length;
+    document.getElementById('lightbox-img').src = images[currentIndex].src;
+}
