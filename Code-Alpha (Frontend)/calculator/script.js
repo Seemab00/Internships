a476b6e5240ed833d21aed7c88debab344294a06
Simaab Malik <seemabmalik2005@gmail.com>
2025-12-29 03:41:37 +0500

    Add files via upload

--- Code-Alpha (Frontend)/calculator/script.js
+++ Code-Alpha (Frontend)/calculator/script.js
@@ -1,1 +1,31 @@
+let display = document.getElementById('display');
+let expression = '';
 
+function append(char) {
+    expression += char;
+    display.value = expression;
+}
+
+function operate(op) {
+    expression += op;
+    display.value = expression;
+}
+
+function clearDisplay() {
+    expression = '';
+    display.value = '';
+}
+
+function calculate() {
+    try {
+        display.value = eval(expression);
+        expression = display.value;
+    } catch (e) {
+        display.value = 'Error';
+    }
+}
+
+
+document.addEventListener('keydown', (e) => {
+    if (e.key >= '0' && e.key <= '9') append(e.key);
+});
